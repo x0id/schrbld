8c6d38522f2dfba73b43b3edfe4f87cc880cc7e0
Dmitriy Kargapolov <dmitriy.kargapolov@gmail.com>
2014-03-10 23:09:59 -0400

    centos roots (with epel) - done

--- mk_centos_roots.sh
+++ mk_centos_roots.sh
@@ -29,6 +29,54 @@ to_32_conf () {
     echo "personality=linux32"
 }
 
+centos_repo_old () {
+    local release=$1
+    local major=${release%%.*}
+    cat <<EOT
+[tmp_centos]
+name=CentOS-\$releasever - Base
+baseurl=http://vault.centos.org/$release/os/\$basearch/
+gpgcheck=1
+gpgkey=http://vault.centos.org/$release/os/\$basearch/RPM-GPG-KEY-centos$major
+multilib_policy=best
+
+[tmp_epel]
+name=Extra Packages for Enterprise Linux $major - \$basearch
+mirrorlist=http://mirrors.fedoraproject.org/mirrorlist?repo=epel-$major&arch=\
+\$basearch
+gpgcheck=1
+gpgkey=https://fedoraproject.org/static/217521F6.txt
+multilib_policy=best
+EOT
+}
+
+centos_repo () {
+    local release=$1
+    local major=${release%%.*}
+    case $major in
+    5*) epelkey=https://fedoraproject.org/static/217521F6.txt ;;
+    6*) epelkey=https://fedoraproject.org/static/0608B895.txt ;;
+    esac
+    cat <<EOT
+[tmp_centos]
+name=CentOS-\$releasever - Base
+mirrorlist=http://mirrorlist.centos.org/?release=\$releasever&arch=\$basearch\
+&repo=os
+gpgcheck=1
+gpgkey=http://mirror.centos.org/centos/\$releasever/os/\$basearch/\
+RPM-GPG-KEY-CentOS-\$releasever
+multilib_policy=best
+
+[tmp_epel]
+name=Extra Packages for Enterprise Linux $major - \$basearch
+mirrorlist=http://mirrors.fedoraproject.org/mirrorlist?repo=epel-$major&arch=\
+\$basearch
+gpgcheck=1
+gpgkey=$epelkey
+multilib_policy=best
+EOT
+}
+
 # create minimum centos chroot
 init_centos () {
     local release=$1
@@ -38,15 +86,19 @@ init_centos () {
     local dev=/dev/$vg/$lv
     lvcreate -L 2G $vg -n $lv
     mkfs.ext4 $dev
+    case $release in
+    4  ) centos_repo_old 4.9 >/etc/yum.repos.d/tmp.repo ;;
+    4.*) centos_repo_old $release >/etc/yum.repos.d/tmp.repo ;;
+      *) centos_repo $release >/etc/yum.repos.d/tmp.repo ;;
+    esac
     if [[ "$bits" == "32" ]]; then
         to_32_conf >${chroot_d}/to_32.conf
-        centos_bootstrap $dev $release |schroot -c to32 -u root
+        centos_bootstrap $dev $release $bits |schroot -c to32 -u root
         rm -f ${chroot_d}/to_32.conf
     else
-        centos_repo >/etc/yum.repos.d/tmp.repo
-        centos_bootstrap $dev $release |bash
-        # rm -f /etc/yum.repos.d/tmp.repo
+        centos_bootstrap $dev $release $bits |bash
     fi
+    rm -f /etc/yum.repos.d/tmp.repo
 }
 
 # write configuration files for created snapshots
@@ -84,31 +136,25 @@ prep_centos () {
     local bits=$2
     local name=centos_${release}_${bits}
     cd /tmp
-    add_rpms |schroot -c source:$name -u root
+    add_rpms $release |schroot -c source:$name -u root
 }
 
 # install basic centos rpms needed to configure, make etc.
 add_rpms () {
+    local release=$1
     local -a pkg
     pkg+=(binutils gcc-c++ make boost-devel)
     pkg+=(tar file diffutils findutils)
     pkg+=(git subversion vim-enhanced)
+    case $release in
+    4*) pkg+=(gcc4-c++ compat-boost-1331-devel boost141-devel) ;;
+    5*) pkg+=(gcc44-c++ binutils220 boost141-devel) ;;
+    esac
 cat <<EOT
     rpm --rebuilddb
+    yum -y clean all
     yum -y update
     yum -y install ${pkg[@]}
-EOT
-}
-
-centos_repo () {
-    cat <<EOT
-[tmp]
-name=CentOS-\$releasever - Base
-mirrorlist=http://mirrorlist.centos.org/?release=\$releasever&arch=\$basearch\
-&repo=os
-gpgcheck=1
-gpgkey=http://mirror.centos.org/centos/\$releasever/os/\$basearch/\
-RPM-GPG-KEY-CentOS-\$releasever
 EOT
 }
 
@@ -116,15 +162,51 @@ EOT
 centos_bootstrap () {
     local dev=$1
     local release=$2
+    local bits=$3
+    local rel
+    local unmount
+    local fixrep
+    case $release in
+    4) rel=4.9 ;;
+    *) rel=$release ;;
+    esac
+    case $release in
+    4*)
+        umount="umount \$mnt/proc; umount \$mnt"
+        fixrep+="for f in \$mnt/etc/yum.repos.d/CentOS*.repo; do "
+        fixrep+="cat \$f |sed -e 's/^mirrorlist/#mirrorlist/g' -e 's/^#"
+        fixrep+="baseurl=http:\/\/mirror.centos.org\/centos\/\$releasever/"
+        fixrep+="baseurl=http:\/\/vault.centos.org\/$rel/g'"
+        fixrep+=" >\$f.fix; mv \$f.fix \$f; done"
+        ;;
+     *)
+        umount="umount \$mnt"
+        fixrep="true"
+        ;;
+    esac
 cat <<EOT
     mnt=\`mktemp -d\`
     mount $dev \$mnt
-    yum -y --releasever=$release --installroot=\$mnt --disablerepo='*' \
-        --enablerepo=tmp install centos-release yum
+    yum -y --releasever=$rel --installroot=\$mnt --disablerepo='*' \
+        --enablerepo=tmp_centos --enablerepo=tmp_epel install \
+        centos-release yum db4-utils epel-release
+    pkg=\`mktemp -d\`
+    cd \$pkg
+    rm -f \$mnt/var/lib/rpm/__*
+    mv \$mnt/var/lib/rpm/* .
+    for f in *; do
+    name=source:centos_${release}_${bits}
+        db_dump \$f |schroot -c \$name -u root -- db_load /var/lib/rpm/\$f
+        rm -f \$f
+    done
+    cd -
+    rmdir \$pkg
+    echo "multilib_policy=best" >> \$mnt/etc/yum.conf
+    $fixrep
     mkdir -p \$mnt/home/build
     chown build:build \$mnt/home/build
     chmod 700 \$mnt/home/build
-    umount \$mnt
+    $umount
     rmdir \$mnt
 EOT
 }
@@ -133,16 +215,13 @@ EOT
 make_centos_root () {
     local release=$1
     local bits=$2
-    init_centos $release $bits
     make_conf_centos $release $bits
+    init_centos $release $bits
     prep_centos $release $bits
 }
 
-# make_centos_root 6 32
-# make_centos_root 6 64
-
 # create multiple centos chroots in a one shot
-for rel in 5; do
+for rel in 4 5 6; do
     for bits in 32 64; do
         make_centos_root $rel $bits
     done
