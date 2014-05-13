391f2846439ccb7593ddc17537df46dc0b9a0a4c
Dmitriy Kargapolov <dmitriy.kargapolov@gmail.com>
2014-05-12 21:15:44 -0400

    fix

--- update.sh
+++ update.sh
@@ -1,9 +1,22 @@
+#!/bin/bash
+# ex: ft=sh ts=4 sw=4 et
+#------------------------------------------------------------------------
+# This script updates Linux schroots
+#------------------------------------------------------------------------
+# Since: 12 May 2014
+#------------------------------------------------------------------------
+# Copyright (C) 2014 Dmitriy Kargapolov <dmitriy.kargapolov@gmail.com>
+# Use, modification and distribution are subject to the Boost Software
+# License, Version 1.0 (See accompanying file LICENSE_1_0.txt or copy
+# at http://www.boost.org/LICENSE_1_0.txt)
+#------------------------------------------------------------------------
 
 update_fedora () {
     local release=$1
     local bits=$2
     local name=fedora_${release}_${bits}
     cd /tmp
+    echo "working on $name"
     echo "yum -y update" |schroot -c source:$name -u root
 }
 
@@ -12,6 +25,7 @@ update_centos () {
     local bits=$2
     local name=centos_${release}_${bits}
     cd /tmp
+    echo "working on $name"
     echo "yum -y update" |schroot -c source:$name -u root
 }
 
@@ -19,12 +33,13 @@ update_arch () {
     local bits=$1
     local name=arch_${bits}
     cd /tmp
+    echo "working on $name"
     echo "pacman --noconfirm -Syu" |schroot -c source:$name -u root
 }
 
 for rel in 15 16 17 18 19 20; do
     for bits in 32 64; do
-        update_fedora
+        update_fedora $rel $bits
     done
 done
 
